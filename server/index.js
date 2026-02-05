39eb1421eea6123d43dad4cd4ac979b6b4982c43
sudhareact <sudhaelango.94@gmail.com>
2026-02-05 11:10:49 +0530

    backend commit

--- server/index.js
+++ server/index.js
@@ -3,14 +3,14 @@ import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
-//import route from './routes/'
+import route from './routes/UserRoute.js'
 const app=express();
 app.use(bodyParser.json());
 app.use(cors());
 dotenv.config();
 const PORT =process.env.PORT || 8009;
-const MONGOURL =process.env.MONGO_URL;
-mongoose.connect(MONGOURL).then(()=>{
+const MONGO_URL =process.env.MONGO_URL;
+mongoose.connect(MONGO_URL).then(()=>{
     console.log("mongodb connection success.....")
    app.listen(PORT,()=>{
     console.log(`server port listening at ${PORT}...`)
@@ -18,3 +18,4 @@ mongoose.connect(MONGOURL).then(()=>{
 }).catch((err)=>{
     console.log("Error",err.toString())
 })
+app.use("/api",route)
